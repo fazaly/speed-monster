8162c3005dd4479ad3a9b36b58e852cea8e048bf
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-09-14 23:33:47 +0600

    countdownOverlay resolved

--- script.js
+++ script.js
@@ -108,7 +108,6 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    // debuging
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
